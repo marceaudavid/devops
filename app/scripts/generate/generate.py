07e0d7295d03b324debb20d5e5ff2537b4cc1eff
AlexSuchot <alexandre.suchot@ynov.com>
2020-04-14 11:48:31 +0200

    Generate all values

--- app/scripts/generate/generate.py
+++ app/scripts/generate/generate.py
@@ -7,7 +7,7 @@ from datetime import datetime
 default_unit_number = 1  # replace this with database value to swap to unit 2, 3... and implement change every 10 robot
 
 
-def generate_unit_number(number):
+def get_unit_number(number):
     if number <= 10:
         number = 1
         return number
@@ -16,42 +16,70 @@ def generate_unit_number(number):
         return number
 
 
-def generate_robot_type():
+def get_robot_type():
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
 
 
-def generate_tank_temperature():
+def get_tank_temperature():
     return round(random.uniform(2.5, 4.1), 1)
 
 
-def external_temperature():
+def get_external_temperature():
     return round(random.uniform(9, 14.1), 1)
 
 
-def generate_weight_of_milk_in_tank():
-    return round(random.uniform(3512, 4607))
+def get_weight_of_milk_in_tank():
+    return round(random.uniform(3512, 4608))
+
+
+def get_ph_measure():
+    return round(random.uniform(6.8, 7.3), 1)
+
+
+def get_k_measure():
+    return round(random.uniform(35, 48))
+
+
+def get_naci_concentration():
+    return round(random.uniform(1, 1.71), 2)
+
+
+def get_salmonella_bacterium_level():
+    return round(random.uniform(17, 38))
+
+
+def get_e_coli_bacterium_level():
+    return round(random.uniform(35, 50))
+
+
+def get_listeria_bacterium_level():
+    return round(random.uniform(28, 55))
 
 
 data = {'robots': []}
 
 
 def generate_a_robot(previous_weight_of_milk, iterate):
-    new_weight_of_milk = generate_weight_of_milk_in_tank()
+    new_weight_of_milk = get_weight_of_milk_in_tank()
     if iterate == 0:
         tank_weight_difference = 0
     else:
         tank_weight_difference = new_weight_of_milk - previous_weight_of_milk
 
-    print("previous weight of milk :", previous_weight_of_milk)
-    print("tank weight difference :", tank_weight_difference)
     data['robots'].append({
-        "Unit number": generate_unit_number(default_unit_number),
+        "Unit number": get_unit_number(default_unit_number),
         "Robot number": iterate + 1,
-        "Robot type": generate_robot_type(),
-        "Tank temperature (Celsius)": generate_tank_temperature(),
-        "External temperature (Celsius)": external_temperature(),
+        "Robot type": get_robot_type(),
+        "Tank temperature (Celsius)": get_tank_temperature(),
+        "External temperature (Celsius)": get_external_temperature(),
         "Weight of milk in tank (Kg)": new_weight_of_milk,
         "Weight ok milk difference (Kg)": tank_weight_difference,
+        "pH measure": get_ph_measure(),
+        "K+ measure (mg/litre)": get_k_measure(),
+        "NaCL concentration (g/litre)": get_naci_concentration(),
+        "Salmonella bacterium level (ppm)": get_salmonella_bacterium_level(),
+        "E-coli bacterium level (ppm)": get_e_coli_bacterium_level(),
+        "Listeria bacterium level (ppm)": get_listeria_bacterium_level(),
     })
 
 
@@ -61,7 +89,7 @@ def generate_json(unit_data):
     timestamp = time.time()
     timestamp_to_string = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H-%M-%S')
 
-    filename = str(generate_unit_number(default_unit_number)) + "-" + timestamp_to_string + ".json"
+    filename = str(get_unit_number(default_unit_number)) + "-" + timestamp_to_string + ".json"
 
     # Generate json file with a unit of 10 robots :
 
@@ -71,8 +99,8 @@ def generate_json(unit_data):
 
 def generate_a_unit():
     for i in range(10):
-        generate_new_weight_of_milk = generate_weight_of_milk_in_tank()
-        generate_a_robot(generate_new_weight_of_milk, i)
+        get_new_weight_of_milk = get_weight_of_milk_in_tank()
+        generate_a_robot(get_new_weight_of_milk, i)
 
 
 generate_a_unit()
