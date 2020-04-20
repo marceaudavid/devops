fcd5a2687915e76f113c12e6701f10345bb435e1
AlexSuchot <alexandre.suchot@ynov.com>
2020-04-20 15:10:39 +0200

    Insert a unit of 10 robots in the database

--- app/scripts/generate/generate.py
+++ app/scripts/generate/generate.py
@@ -2,9 +2,16 @@ import json
 import random
 import string
 import time
+import mysql.connector
 from datetime import datetime
 
 default_unit_number = 1  # replace this with database value to swap to unit 2, 3... and implement change every 10 robot
+
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='127.0.0.1',
+                              database='devops')
+
+cursor = cnx.cursor()
 
 
 def get_unit_number(number):
@@ -59,28 +66,89 @@ def get_listeria_bacterium_level():
 data = {'robots': []}
 
 
-def generate_a_robot(previous_weight_of_milk, iterate):
-    new_weight_of_milk = get_weight_of_milk_in_tank()
+def generate_a_robot(iterate):
+    weight_of_milk_in_tank = get_weight_of_milk_in_tank()
+
     if iterate == 0:
-        tank_weight_difference = 0
+        weight_of_milk_difference = 0
     else:
-        tank_weight_difference = new_weight_of_milk - previous_weight_of_milk
+        # Faire un select :récupérer weight_of_milk_in_tank du robot précédent et remplacer -1 par cette value
+        weight_of_milk_difference = weight_of_milk_in_tank - 1
+
+    unit_number = get_unit_number(default_unit_number)
+    robot_number = iterate + 1
+    robot_type = get_robot_type()
+    tank_temperature = get_tank_temperature()
+    external_temperature = get_external_temperature()
+    ph_measure = get_ph_measure()
+    k_measure = get_k_measure()
+    naci_concentration = get_naci_concentration()
+    salmonella_bacterium_level = get_salmonella_bacterium_level()
+    e_coli_bacterium_level = get_e_coli_bacterium_level()
+    listeria_bacterium_level = get_listeria_bacterium_level()
 
     data['robots'].append({
-        "Unit number": get_unit_number(default_unit_number),
-        "Robot number": iterate + 1,
-        "Robot type": get_robot_type(),
-        "Tank temperature (Celsius)": get_tank_temperature(),
-        "External temperature (Celsius)": get_external_temperature(),
-        "Weight of milk in tank (Kg)": new_weight_of_milk,
-        "Weight ok milk difference (Kg)": tank_weight_difference,
-        "pH measure": get_ph_measure(),
-        "K+ measure (mg/litre)": get_k_measure(),
-        "NaCL concentration (g/litre)": get_naci_concentration(),
-        "Salmonella bacterium level (ppm)": get_salmonella_bacterium_level(),
-        "E-coli bacterium level (ppm)": get_e_coli_bacterium_level(),
-        "Listeria bacterium level (ppm)": get_listeria_bacterium_level(),
+        "Unit number": unit_number,
+        "Robot number": robot_number,
+        "Robot type": robot_type,
+        "Tank temperature (Celsius)": tank_temperature,
+        "External temperature (Celsius)": external_temperature,
+        "Weight of milk in tank (Kg)": weight_of_milk_in_tank,
+        "Weight ok milk difference (Kg)": weight_of_milk_difference,
+        "pH measure": ph_measure,
+        "K+ measure (mg/litre)": k_measure,
+        "NaCL concentration (g/litre)": naci_concentration,
+        "Salmonella bacterium level (ppm)": salmonella_bacterium_level,
+        "E-coli bacterium level (ppm)": e_coli_bacterium_level,
+        "Listeria bacterium level (ppm)": listeria_bacterium_level,
     })
+
+    data_robot = {
+        "unit_number": unit_number,
+        "robot_number": robot_number,
+        "robot_type": robot_type,
+        "tank_temperature": tank_temperature,
+        "external_temperature": external_temperature,
+        "weight_of_milk_in_tank": weight_of_milk_in_tank,
+        "weight_of_milk_difference": weight_of_milk_difference,
+        "ph_measure": ph_measure,
+        "k_measure": k_measure,
+        "naci_concentration": naci_concentration,
+        "salmonella_bacterium_level": salmonella_bacterium_level,
+        "e_coli_bacterium_level": e_coli_bacterium_level,
+        "listeria_bacterium_level": listeria_bacterium_level,
+    }
+
+    add_robot = ("INSERT INTO robots "
+                    "(unit_number,"
+                    "robot_number,"
+                    "robot_type,"
+                    "tank_temperature,"
+                    "external_temperature,"
+                    "weight_of_milk_in_tank,"
+                    "weight_of_milk_difference,"
+                    "ph_measure,"
+                    "k_measure,"
+                    "naci_concentration,"
+                    "salmonella_bacterium_level,"
+                    "e_coli_bacterium_level) "
+                    "VALUES ("
+                    "%(unit_number)s,"
+                    "%(robot_number)s,"
+                    "%(robot_type)s,"
+                    "%(tank_temperature)s,"
+                    "%(external_temperature)s,"
+                    "%(weight_of_milk_in_tank)s,"
+                    "%(weight_of_milk_difference)s,"
+                    "%(ph_measure)s,"
+                    "%(k_measure)s,"
+                    "%(naci_concentration)s,"
+                    "%(salmonella_bacterium_level)s,"
+                    "%(e_coli_bacterium_level)s"
+                    ")"
+                 )
+    cursor.execute(add_robot, data_robot)
+    cnx.commit()
 
 
 def generate_json(unit_data):
@@ -99,9 +167,10 @@ def generate_json(unit_data):
 
 def generate_a_unit():
     for i in range(10):
-        get_new_weight_of_milk = get_weight_of_milk_in_tank()
-        generate_a_robot(get_new_weight_of_milk, i)
+        generate_a_robot(i)
 
 
 generate_a_unit()
 generate_json(data)
+cursor.close()
+cnx.close()
