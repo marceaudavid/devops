105b7cc31e7ed5dc1cdec9c775b5fdf05235ca72
CorentinGlrd5 <corentin.guillard@ynov.com>
2020-04-27 17:13:37 +0200

    fix(dev): add functionArrow and change style css

--- app/web/src/App.vue
+++ app/web/src/App.vue
@@ -6,6 +6,11 @@
       <div class="container-graph">
         <Graph />
         <GraphRobot1 />
+        <div class="contenent-arrow">
+          <a @click="functionArrow()" id="arrow">
+            <img class="arrow" src="./assets/chevron-down-outline.svg" alt="chevron-down-outline" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -24,6 +29,38 @@ export default {
     NavBarTop,
     Graph,
     GraphRobot1
+  },
+  methods: {
+    functionArrow() {
+      //récuperer la hauteur de ma page
+      // var h = window.scrollMaxY + document.body.offsetHeight;
+
+      //récuper la hauteur de ma fenetre
+      // var hauteur = window.innerHeight;
+
+      // Récuperer la balise fleche du fichier html
+      var x = document.getElementById("arrow");
+
+      // Récuperer la valeur px du body en fonction du haut de la fenetre de l'utilisateur
+      var top = window.scrollY;
+      console.log(top);
+
+      if (top >= 0 && top <= 736) {
+        window.scrollTo(0, 1000);
+        x.style.transform = "rotate(180deg)";
+      }
+      // if (top >= 728 && top <= 1456) {
+      //   window.scrollTo(0, 1456);
+      // }
+      // if (top >= 1456 && top <= 2184) {
+      //   window.scrollTo(0, 2184);
+      //   x.style.transform = "rotate(180deg)";
+      // }
+      if (top >= 736 && top <= 736) {
+        window.scrollTo(0, 0);
+        x.style.transform = "rotate(0deg)";
+      }
+    }
   }
 };
 </script>
@@ -45,5 +82,28 @@ export default {
 }
 .container-graph {
   width: 85%;
+}
+.contenent-arrow {
+  position: fixed;
+  bottom: 0;
+  width: 81%;
+  height: 30px;
+  display: flex;
+  align-content: center;
+  justify-content: flex-end;
+  margin: 2rem;
+}
+#arrow {
+  width: 30px;
+  height: 30px;
+  transform: rotate(0deg);
+  transition: transform 0.5s;
+  background-color: #4eb4a8;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.3);
+}
+.arrow {
+  width: 100%;
+  height: 100%;
 }
 </style>
