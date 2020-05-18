6712eb8da9c16cb19979a93d26751b661969985c
AlexSuchot <alexandre.suchot@ynov.com>
2020-05-18 16:43:17 +0200

    Socket TCP : multithreading

--- app/scripts/server/server.py
+++ app/scripts/server/server.py
@@ -1,24 +1,13 @@
 import os
 import pickle
+import select
 import socket
-import time
 
 import mysql.connector
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-
 from dotenv import load_dotenv
 
-time.sleep(15)
-
 load_dotenv()
-
-# Create a socket (SOCK_STREAM means a TCP socket)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    server_socket.bind((socket.gethostname(), 5000))
-except socket.error:
-    print("The socket can't bind to the chosen address")
 
 cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'],
                               host=os.environ['MYSQL_HOST'], database=os.environ['MYSQL_DATABASE'])
@@ -26,7 +15,7 @@ cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ
 cursor = cnx.cursor(buffered=True)
 
 
-def set_robot_data(y):
+def set_robot_data(data_dict, y):
     unit_number = data_dict.get("robots", {})[y].get("Unit number")
     robot_number = data_dict.get("robots", {})[y].get("Robot number")
     robot_type = data_dict.get("robots", {})[y].get("Robot type")
@@ -60,53 +49,77 @@ def set_robot_data(y):
     return data_robot
 
 
-data_dict = ""
+def main() -> None:
+    host = socket.gethostname()
+    port = 5000
+    data_dict = ""
 
-while True:
-    print("Server is ready :")
-    server_socket.listen(5)
-    clientsocket, address = server_socket.accept()
+    # create a TCP/IP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
-    data = clientsocket.recv(195000)
-    data_dict = pickle.loads(data)
+        # bind the socket to the port
+        sock.bind((host, port))
+        # listen for incoming connections
+        sock.listen(5)
+        print("Server started...")
 
-    for y in range(0, 10):
-        add_robot = ("INSERT INTO robots "
-                     "(unit_number,"
-                     "robot_number,"
-                     "robot_type,"
-                     "tank_temperature,"
-                     "external_temperature,"
-                     "weight_of_milk_in_tank,"
-                     "ph_measure,"
-                     "k_measure,"
-                     "nacl_concentration,"
-                     "salmonella_bacterium_level,"
-                     "e_coli_bacterium_level,"
-                     "listeria_bacterium_level,"
-                     "creation_time)"
-                     "VALUES ("
-                     "%(unit_number)s,"
-                     "%(robot_number)s,"
-                     "%(robot_type)s,"
-                     "%(tank_temperature)s,"
-                     "%(external_temperature)s,"
-                     "%(weight_of_milk_in_tank)s,"
-                     "%(ph_measure)s,"
-                     "%(k_measure)s,"
-                     "%(nacl_concentration)s,"
-                     "%(salmonella_bacterium_level)s,"
-                     "%(e_coli_bacterium_level)s,"
-                     "%(listeria_bacterium_level)s,"
-                     "%(creation_time)s"
-                     ")"
-                     )
+        # sockets from which we expect to read
+        inputs = [sock]
+        outputs = []
 
-        data_robot = set_robot_data(y)
-        set_robot_data(y)
+        while inputs:
+            # wait for at least one of the sockets to be ready for processing
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)
 
-        cursor.execute(add_robot, data_robot)
-        cnx.commit()
+            for s in readable:
+                if s is sock:
+                    conn, addr = s.accept()
+                    inputs.append(conn)
+                else:
+                    data = s.recv(4096)
+                    if data:
+                        data_dict = pickle.loads(data)
+                        for y in range(10):
+                            add_robot = ("INSERT INTO robots "
+                                         "(unit_number,"
+                                         "robot_number,"
+                                         "robot_type,"
+                                         "tank_temperature,"
+                                         "external_temperature,"
+                                         "weight_of_milk_in_tank,"
+                                         "ph_measure,"
+                                         "k_measure,"
+                                         "nacl_concentration,"
+                                         "salmonella_bacterium_level,"
+                                         "e_coli_bacterium_level,"
+                                         "listeria_bacterium_level,"
+                                         "creation_time)"
+                                         "VALUES ("
+                                         "%(unit_number)s,"
+                                         "%(robot_number)s,"
+                                         "%(robot_type)s,"
+                                         "%(tank_temperature)s,"
+                                         "%(external_temperature)s,"
+                                         "%(weight_of_milk_in_tank)s,"
+                                         "%(ph_measure)s,"
+                                         "%(k_measure)s,"
+                                         "%(nacl_concentration)s,"
+                                         "%(salmonella_bacterium_level)s,"
+                                         "%(e_coli_bacterium_level)s,"
+                                         "%(listeria_bacterium_level)s,"
+                                         "%(creation_time)s"
+                                         ")"
+                                         )
 
-    if data_dict != "":
-        data_dict = ""
+                            data_robot = set_robot_data(data_dict, y)
+                            set_robot_data(data_dict, y)
+
+                            cursor.execute(add_robot, data_robot)
+                            cnx.commit()
+                    else:
+                        inputs.remove(s)
+                        s.close()
+
+
+if __name__ == "__main__":
+    main()
