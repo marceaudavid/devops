650d08c318633e6a3017e6ed3ee6723ecacae961
marceaudavid <marceau.david@ynov.com>
2020-05-26 08:31:26 +0200

    Fix dump script connect string

--- app/database/dump.py
+++ app/database/dump.py
@@ -9,14 +9,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 USER = os.environ['MYSQL_ADMIN_DELEGATE_USER']
-PASSWORD = os.environ['MYSQL_ADMIN_DELEGATE_PASSWORD']
+PASSWORD = os.environ['MYSQL_ADMIN_DELEGATE_USER_PASSWORD']
 HOST = os.environ['MYSQL_HOST']
 DATABASE = os.environ['MYSQL_DATABASE']
 
-BACK_UP_TIME = 86400.0 # DEFAULT 86400 = 1 Day backup
+BACK_UP_TIME = 86400.0  # DEFAULT 86400 = 1 Day backup
 
-cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'],
-                              host=os.environ['MYSQL_HOST'], database=os.environ['MYSQL_DATABASE'])
+cnx = mysql.connector.connect(user="admin_delegate", password="admin_delegate",
+                              host="localhost", database="devops")
 
 cursor = cnx.cursor(buffered=True)
 
@@ -25,8 +25,9 @@ def generate_file():
     datetime = time.strftime('%d-%m-%Y-%H%M%S')
     filename = datetime + ".sql"
     file = open(filename, "x")
-    dump = "mysqldump -h " + HOST + " -P 3306" + " -u " + USER + " " + DATABASE + ' > ' + filename
-    threading.Timer(86400.0, generate_file).start()
+    dump = "mysqldump -h " + HOST + " -P 3306" + \
+        " -u " + USER + " " + DATABASE + ' > ' + filename
+    threading.Timer(10, generate_file).start()
 
 
 generate_file()
